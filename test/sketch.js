6db99c0068bfe2aa01f64a8d87d4ac488718bd86
jcvargas10 <36166613+jcvargas10@users.noreply.github.com>
2018-02-24 22:18:31 -0500

    full test

--- test/sketch.js
+++ test/sketch.js
@@ -1,47 +1,32 @@
+var shwtm = 'OPEN ON MOBILE DEVICE';
+
 function setup() {
-  createCanvas(400, 400);
-  //textFont('monospace');
-  textSize(20);
+	createCanvas(windowWidth, windowHeight);
+
+	textFont('monospace');
+	textAlign(CENTER, CENTER);
 }
 
 function draw() {
-  //background(220);
-  text(deviceOrientation, 0, 0);
-  text(accelerationX, 0, 20);
-  text(accelerationY, 0, 40);
-  text(accelerationZ, 0, 60);
-  text(pAccelerationX, 0, 80);
-  text(pAccelerationY, 0, 100);
-  text(pAccelerationZ, 0, 120);
-  text(rotationX, 0, 140);
-  text(rotationY, 0, 160);
-  text(rotationZ, 0, 180);
-  text(pRotationX, 0, 200);
-  text(pRotationY, 0, 220);
-  text(pRotationZ, 0, 240);
-  text(turnAxis, 0, 260);
-}
-
-function deviceMoved() {
-  fill(255, 0, 0);
-  rect(200, 0, 100, 100);
+	background(255);
+	if (turnAxis != undefined) {
+		text(shwtm, width / 2, height / 2);
+	} else {
+		text(shwtm, width / 2, 10);
+	}
 }
 
 function deviceTurned() {
-  if (turnAxis === 'X')
-    fill(255, 0, 0);
-  
-  if (turnAxis === 'Y')
-    fill(0, 255, 0);
-  
-  if (turnAxis === 'Z')
-    fill(0, 0, 255);
-  
-  rect(200, 100, 100, 100);
-  fill(0);
+	if (turnAxis === 'X')
+		shwtm = ' ' + hour() + ':';
+
+	if (turnAxis === 'Y')
+		shwtm = ':' + minute() + ':';
+
+	if (turnAxis === 'Z')
+		shwtm = ':' + second() + ' ';
 }
 
-function deviceShaken() {
-  fill(0, 0, 255);
-  rect(200, 100, 100, 100);
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
 }
